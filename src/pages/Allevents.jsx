eae9e09468b8a84bc6c2cc5a91d7b248ec6b80b8
AMIREDDYRAKEY <rakeyr213@gmail.com>
2025-08-05 23:47:58 +0530

    message

--- src/pages/Allevents.jsx
+++ src/pages/Allevents.jsx
@@ -53,7 +53,7 @@ const Allevents =  () => {
       }
   }
     return (
-         <div className='min-h-screen w-full relative'>
+         <div className='min-h-screen w-full relative z-[100]'>
       <div className='w-full px-4 md:px-8 pt-8'>
         <h3 className='text-[#74779a] font-bold text-3xl text-center mb-8'>All Events</h3>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center'>
